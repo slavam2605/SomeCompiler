45aef477071287632793c29ef47923190b54616e
slava <slavam2605@bk.ru>
2018-10-04 00:03:16 +0300

    Implement util method for parsing expressions

--- src/moklev/compiler/parsing/ParserUtil.kt
+++ src/moklev/compiler/parsing/ParserUtil.kt
@@ -1,6 +1,7 @@
 package moklev.compiler.parsing
 
 import moklev.compiler.ast.ASTNode
+import moklev.compiler.ast.ExpressionASTNode
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
@@ -8,11 +9,19 @@ import org.antlr.v4.runtime.CommonTokenStream
  * @author Moklev Vyacheslav
  */
 object ParserUtil {
-    fun parseString(contents: String): ASTNode {
+    fun parse(contents: String): ASTNode {
         val stream = CharStreams.fromString(contents)
         val lexer = SomeLexer(stream)
         val tokenStream = CommonTokenStream(lexer)
         val parser = SomeParser(tokenStream)
         return parser.file().result
+    }
+    
+    fun parseExpression(contents: String): ExpressionASTNode {
+        val stream = CharStreams.fromString(contents)
+        val lexer = SomeLexer(stream)
+        val tokenStream = CommonTokenStream(lexer)
+        val parser = SomeParser(tokenStream)
+        return parser.expression().result
     }
 }
