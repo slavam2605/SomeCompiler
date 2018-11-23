60f777aed61be3c4c993e1733f71c257b95efc37
Vyacheslav Moklev <slavam2605@bk.ru>
2018-11-23 17:40:55 +0300

    Add tests for class methods

--- test/src/moklev/integrational/evaluation/ClassesTest.kt
+++ test/src/moklev/integrational/evaluation/ClassesTest.kt
@@ -92,4 +92,69 @@ class ClassesTest : EvaluationTestBase() {
                 "Class<TestResult>{Class<Pair>{Int64[10], Int64[20]}, Class<Pair>{Int64[11], Int64[21]}, Class<Pair>{Int64[12], Int64[22]}, Class<Pair>{Int64[13], Int64[23]}}"
         )
     }
+
+    @Test
+    fun testClassMethods() {
+        runTest(
+                """
+                    class Pair {
+                        var x: int64;
+                        var y: int64;
+
+                        fun sum(): int64 {
+                            return *this.x + *this.y;
+                        }
+                    }
+                """,
+                "Pair(11, 20).sum()",
+                "Int64[31]"
+        )
+        runMultipleTests(
+                """
+                    class Complex {
+                        var re: int64;
+                        var im: int64;
+
+                        fun sum(other: Complex): Complex* {
+                            *this.re = *this.re + other.re;
+                            *this.im = *this.im + other.im;
+                            return this;
+                        }
+
+                        fun sub(other: Complex): Complex* {
+                            *this.re = *this.re - other.re;
+                            *this.im = *this.im - other.im;
+                            return this;
+                        }
+
+                        fun mul(other: Complex): Complex* {
+                            var newRe: int64;
+                            var newIm: int64;
+                            newRe = *this.re * other.re - *this.im * other.im;
+                            newIm = *this.re * other.im + *this.im * other.re;
+                            *this.re = newRe;
+                            *this.im = newIm;
+                            return this;
+                        }
+                    }
+
+                    fun test1(): Complex {
+                        var a: Complex;
+                        a = Complex(1, 1);
+                        return *(a.mul(Complex(1, 1)));
+                    }
+
+                    fun test2(): Complex {
+                        var a: Complex;
+                        a = Complex(1, 1);
+                        a.sum(Complex(1, 1));
+                        a.mul(Complex(1, 1));
+                        a.sub(Complex(1, 1));
+                        return a;
+                    }
+                """,
+                listOf("test1()", "test2()"),
+                listOf("Class<Complex>{Int64[0], Int64[2]}", "Class<Complex>{Int64[-1], Int64[3]}")
+        )
+    }
 }
--- test/src/moklev/integrational/evaluation/EvaluationTestBase.kt
+++ test/src/moklev/integrational/evaluation/EvaluationTestBase.kt
@@ -11,12 +11,21 @@ import kotlin.test.assertEquals
  */
 abstract class EvaluationTestBase {
     protected fun runTest(program: String, testExpression: String, expectedResult: String) {
+        runMultipleTests(program, listOf(testExpression), listOf(expectedResult))
+    }
+
+    protected fun runMultipleTests(program: String, testExpressions: List<String>, expectedResults: List<String>) {
         val programAst = ParserUtil.parse(program)
-        val expressionAst = ParserUtil.parseExpression(testExpression)
         val builder = SemanticBuilder()
         builder.build(programAst, TopLevelContext)
-        val expression = builder.buildExpression(expressionAst, TopLevelContext)
-        val result = Evaluator().evaluateExpression(expression).toString()
-        assertEquals(expectedResult, result)
+        assertEquals(testExpressions.size, expectedResults.size, "Expression and result lists are differ in length")
+        for (index in 0 until testExpressions.size) {
+            val testExpression = testExpressions[index]
+            val expectedResult = expectedResults[index]
+            val expressionAst = ParserUtil.parseExpression(testExpression)
+            val expression = builder.buildExpression(expressionAst, TopLevelContext)
+            val result = Evaluator().evaluateExpression(expression).toString()
+            assertEquals(expectedResult, result)
+        }
     }
 }
